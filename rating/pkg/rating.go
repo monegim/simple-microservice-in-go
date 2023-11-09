df70a4017f128aefa3b8036ce66d0f553200ff25
Mostafa Negim <mostafa.negim@gmail.com>
2023-11-09 21:38:44 +0330

    Add: rating.go

---
+++ rating/pkg/rating.go
@@ -0,0 +1,18 @@
+package pkg
+
+type RecordID string
+
+type RecordType string
+
+const RecordTypeMovie = RecordType("Movie")
+
+type UserID string
+
+type RatingValue int
+
+type Rating struct {
+	RecordID   RecordID    `json:"recordID"`
+	RecordType RecordType  `json:"recordType"`
+	UserID     UserID      `json:"userId"`
+	Value      RatingValue `json:"value"`
+}
