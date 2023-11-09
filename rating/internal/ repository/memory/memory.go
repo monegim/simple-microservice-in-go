92f28ebca4efa70a9b316bc9aa3954dd12f22a60
Mostafa Negim <mostafa.negim@gmail.com>
2023-11-09 21:57:21 +0330

    Add: Put method

--- rating/internal/ repository/memory/memory.go
+++ rating/internal/ repository/memory/memory.go
@@ -26,3 +26,12 @@ func (r *Repository) Get(ctx context.Context,
 	return r.data[recordType][recordID], nil
 
 }
+
+func (r *Repository) Put(ctx context.Context, recordID model.RecordID,
+	recordType model.RecordType, rating *model.Rating) error {
+	if _, ok := r.data[recordType]; !ok {
+		r.data[recordType] = map[model.RecordID][]model.Rating{}
+	}
+	r.data[recordType][recordID] = append(r.data[recordType][recordID], *rating)
+	return nil
+}
