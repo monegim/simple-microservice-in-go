87d32e38bc9728624ec360992a82164cf0ca91fc
Mostafa Negim <mostafa.negim@gmail.com>
2023-11-09 15:20:29 +0330

    Add: memory.go

---
+++ metadata/internal/repository/memory/memory.go
@@ -0,0 +1,37 @@
+package memory
+
+import (
+	"context"
+	"sync"
+
+	"github.com/monegim/simple-microservice-in-go/metadata/internal/repository"
+	model "github.com/monegim/simple-microservice-in-go/metadata/pkg"
+)
+
+type Repository struct {
+	sync.RWMutex
+	data map[string]*model.Metadata
+}
+
+func New() *Repository {
+	return &Repository{
+		data: map[string]*model.Metadata{},
+	}
+}
+
+func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) {
+	r.RLock()
+	defer r.RUnlock()
+	m, ok := r.data[id]
+	if !ok {
+		return nil, repository.ErrNotFound
+	}
+	return m, nil
+}
+
+func (r *Repository) Put(_ context.Context, id string, metadata *model.Metadata) error {
+	r.Lock()
+	defer r.Unlock()
+	r.data[id] = metadata
+	return nil
+}
