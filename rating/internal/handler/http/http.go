97dcda0c5c60f45fd960f6150aa5e7ec7790129e
Mostafa Negim <mostafa.negim@gmail.com>
2023-12-22 19:43:53 +0330

    Add: Handle func

--- rating/internal/handler/http/http.go
+++ rating/internal/handler/http/http.go
@@ -1,6 +1,14 @@
 package http
 
-import "github.com/monegim/simple-microservice-in-go/rating/internal/controller/rating"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/monegim/simple-microservice-in-go/rating/internal/controller/rating"
+	"github.com/monegim/simple-microservice-in-go/rating/pkg/model"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 type Handler struct {
 	ctrl *rating.Controller
@@ -8,4 +16,47 @@ type Handler struct {
 
 func New(ctrl *rating.Controller) *Handler {
 	return &Handler{ctrl}
+}
+
+// Handle handles PUT and GET /rating requests.
+func (h *Handler) Handle(w http.ResponseWriter, req *http.Request) {
+	recordID := model.RecordID(req.FormValue("id"))
+	if recordID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	recordType := model.RecordType(req.FormValue("type"))
+	if recordType == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	switch req.Method {
+	case http.MethodGet:
+		v, err := h.ctrl.GetAggregateRating(req.Context(), recordID, recordType)
+		if err != nil && errors.Is(err, rating.ErrNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if err := json.NewEncoder(w).Encode(v); err != nil {
+			log.Printf("Response encode error: %v\n", err)
+		}
+	case http.MethodPut:
+		userID := model.UserID(req.FormValue("userId"))
+		v, err := strconv.ParseFloat(req.FormValue("value"), 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := h.ctrl.PutRating(req.Context(), recordID, recordType, &model.Rating{
+			RecordID:   recordID,
+			RecordType: recordType,
+			UserID:     userID,
+			Value:      model.RatingValue(v),
+		}); err != nil {
+			log.Printf("Repository put error: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
