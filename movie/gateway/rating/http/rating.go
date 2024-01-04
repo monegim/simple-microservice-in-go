50fc784aa991035976ad416fa6d54c4dfa155f38
Mostafa Negim <mostafa.negim@gmail.com>
2024-01-04 18:51:20 +0330

    Add: GetAggregatedRating method

---
+++ movie/gateway/rating/http/rating.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	gateway "github.com/monegim/simple-microservice-in-go/movie/internal/ gateway"
+	"github.com/monegim/simple-microservice-in-go/rating/pkg/model"
+	"net/http"
+)
+
+type Gateway struct {
+	addr string
+}
+
+func New(addr string) *Gateway {
+	return &Gateway{addr}
+}
+
+func (g *Gateway) GetAggregatedRating(ctx context.Context,
+	recordID model.RecordID, recordType model.RecordType) (float64, error) {
+	req, err := http.NewRequest(http.MethodGet, g.addr+"/rating", nil)
+	if err != nil {
+		return 0, nil
+	}
+	req = req.WithContext(ctx)
+	values := req.URL.Query()
+	values.Add("id", string(recordID))
+	values.Add("type", fmt.Sprintf("%v", recordType))
+	req.URL.RawQuery = values.Encode()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound {
+		return 0, gateway.ErrNotFound
+	} else if resp.StatusCode/100 != 2 {
+		return 0, fmt.Errorf("non-2xx response: %v", resp)
+	}
+	var v float64
+}
